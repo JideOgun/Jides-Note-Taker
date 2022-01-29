440810f632d8454f7f0cd69995697702d6725b9a
JideOgun <JideOgun@github.com>
2022-01-28 20:36:22 -0600

    reordered html routes fixed bug with /notes not going to the right place

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,18 +2,22 @@ const path = require('path');
 const router = require('express').Router();
 
 
-//ADDING A WILCARD ROUTE I.E ROUTES THAT DO NOT EXIST
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+
+// Route for the home page
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
 });
+
 
 // Route for the notes page
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../../Develop/public/notes.html'));
 });
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+
+//ADDING A WILCARD ROUTE I.E ROUTES THAT DO NOT EXIST
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../Develop/public/index.html'));
 });
 
 
